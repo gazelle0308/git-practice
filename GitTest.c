c336901244feb089ee25a78f3db20b0ff76d803d
gazelle0308 <zowo0056@gmail.com>
2026-07-03 20:45:23 +0900

    Used VC To Git Test

---
+++ GitTest.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+typedef struct Linked_List{
+
+    int data;
+    struct Linked_List* R_Node;
+    struct Linked_List* L_Node;
+
+} Linked_List;
+
+void main() {
+
+    Linked_List* head = NULL;
+
+    head = insert_node(10, NULL, NULL);
+    head = insert_node(20, NULL, head);
+    head = insert_node(30, NULL, head);
+
+    printf("Hello, Git!\n");
+
+    return;
+}
+
+Linked_List* insert_node(int data, Linked_List* R_Node, Linked_List* L_Node) {
+
+    Linked_List* new_node = (Linked_List*)malloc(sizeof(Linked_List));
+    
+    if (new_node == NULL) {
+    
+        printf("Memory allocation failed\n");
+        return NULL;
+    
+    }
+
+    new_node->data = data;
+    if (R_Node != NULL) {
+        new_node->R_Node = R_Node;
+        R_Node->L_Node = new_node;
+    } else {
+        new_node->R_Node = NULL;
+    }
+    if (L_Node != NULL) {
+        new_node->L_Node = L_Node;
+        L_Node->R_Node = new_node;
+    } else {
+        new_node->L_Node = NULL;
+    }
+    return new_node;
+
+}
+
+Linked_List* delete_node(Linked_List* node) {
+
+    if (node == NULL) {
+        return NULL;
+    }
+
+    Linked_List* next_node = node->R_Node;
+    free(node);
+    return next_node;
+
+}
